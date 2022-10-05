789d0a68b4acccbcd419285a1955fada732633c5
Dahlia <rlally89@gmail.com>
2022-10-04 23:13:03 -0700

    add readme

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,17 @@
 // There is a page title, yes!
 // There is a start button, yes!
 // Listen for the start button click, yes!
-// When start clicked, a timer starts counting down for 60 seconds, yes!
+// When start clicked, a timer starts counting down for 20 seconds, yes!
 // When clicked, first question displays with four possible answers, yes!
-// Correct answer, next question
-// Incorrect answer, time subtracted
-// Game ends when all questions are answered or clocks reaches 0
-// Game wins and losses are tallied, saved in localStorage, and rendered on page
+// Correct answer, next question, yes!
+// Incorrect answer, time subtracted, yes!
+// Game ends when all questions are answered or clocks reaches 0, mostly!
+// Game wins and losses are tallied, saved in localStorage, and rendered on page, workinprogress!
 
 var timerEl = document.querySelector("#timer-display");
 var startButton = document.querySelector("#start-button");
 var questionBox = document.querySelector("#questions");
+var scoreBox = document.querySelector("#score");
 
 var choice1 = document.querySelector("#choices1");
 var choice2 = document.querySelector("#choices2");
@@ -18,14 +19,12 @@ var choice3 = document.querySelector("#choices3");
 var choice4 = document.querySelector("#choices4");
 var choiceButton = document.getElementsByClassName(".choice-button");
 
-var right = 0;
-var wrong = 0;
-
+var index = 0;
 
 //variable to keep track of score
-var score = 0;
+var highscore = 0;
 
-var secondsLeft = 30;
+var secondsLeft = 20;
 
 //create an array for quiz questions, answers, and correct answer
 var quiz = [
@@ -54,12 +53,15 @@ console.log(quiz);
 //game function starts when start button clicked
 function startGame () {
     index = 0;
-    timerCount = 30;
+    highscore = 0;
+    secondsLeft = 20;
     startButton.disabled = true;
     renderQuestion()
+    scoreTracker()
     addEventListener()
     countdown()
     console.log('playing!')
+    
 
 }
 
@@ -73,6 +75,7 @@ function addEventListener (){
 
             if (chosenAnswer.textContent === quiz[index].correct) {
             console.log('correct!');
+            highscore++;
             } else {
             console.log('wrong');
             secondsLeft-=5;
@@ -92,6 +95,7 @@ function addEventListener (){
             
             if (chosenAnswer.textContent === quiz[index].correct) {
             console.log('correct!');
+            highscore++;
             } else {
             console.log('wrong');
             secondsLeft-=5;
@@ -110,6 +114,7 @@ function addEventListener (){
             
             if (chosenAnswer.textContent === quiz[index].correct) {
             console.log('correct!');
+            highscore++;
             } else {
             console.log('wrong');
             secondsLeft-=5;
@@ -128,6 +133,7 @@ function addEventListener (){
             
             if (chosenAnswer.textContent === quiz[index].correct) {
             console.log('correct!');
+            highscore++;
             } else {
             console.log('wrong');
             secondsLeft-=5;
@@ -135,14 +141,14 @@ function addEventListener (){
 
             index++;
             renderQuestion();
+          
 
         }
     );
 }
  
                
-var index = 0
-
+//render question to page
 function renderQuestion() {
     var chosenQuestion = quiz[index].question;
     questionBox.textContent = chosenQuestion;
@@ -150,6 +156,13 @@ function renderQuestion() {
     choice2.textContent = quiz[index].answers[1];
     choice3.textContent = quiz[index].answers[2];
     choice4.textContent = quiz[index].answers[3];
+}
+
+//track score, post to local storage
+function scoreTracker () {
+    var finalScore = highscore;
+    scoreBox.textContent = finalScore;
+    localStorage.setItem("highscore", highscore);
 }
  
 //timer function
@@ -159,8 +172,9 @@ function countdown() {
         timerEl.textContent = secondsLeft;
         console.log('timer!')
     
-    if (secondsLeft === 0) {
+    if (secondsLeft <= 0) {
         clearInterval(timeInterval);
+        alert("Game Over")
     }
 }, 1000);
 
